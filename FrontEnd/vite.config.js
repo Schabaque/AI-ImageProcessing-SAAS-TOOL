3c4c822f9d5bdc98e085be0cbdadcc6751ccfa52
sabhay02 <sabhaythakkar02@gmail.com>
2025-10-07 01:22:01 +0530

    Updated package.json

--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -1,7 +1,6 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
-import tailwindcss from "@tailwindcss/vite";
 import { config } from 'dotenv';
 
 // Load environment variables from .env file
@@ -10,7 +9,7 @@ config();
 
 
 export default defineConfig({
-  plugins: [react(),   tailwindcss(),],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
